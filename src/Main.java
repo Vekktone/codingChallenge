d68593fd9587a6979780c9b1cb3db6eadbacb8a2
Marfin,Riley E <marfin.riley@heb.com>
2019-01-17 15:34:49 -0600

    modularized code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,23 +7,46 @@ import java.util.Comparator;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("Histogram coding challenge");
+        Scanner scanner = initializeScanner();
+        List<Word> wordList = createSortedList(scanner);
+        printHistogram(wordList);
+
+    }
+
+    private static int checkListForWord(List<Word> list, String word){
+        for (Word w: list){
+            if (w.getWord().equals(word)){
+                return list.indexOf(w);
+            }
+        }
+
+        return -1;
+    }
+
+    private static void printHistogram(List<Word> wordList) throws IOException {
+        StringBuilder s = new StringBuilder();
+        for (Word w: wordList){
+
+            s.append(String.format("%8s | ", w.getWord()));
+            for (int i = 0; i < w.getCount(); i++){
+                s.append("=");
+            }
+            s.append(" (").append(w.getCount()).append(")\n");
+        }
+
+        PrintWriter out = new PrintWriter("output.txt");
+        out.write(s.toString());
+        out.close();
+
+    }
+
+    private static List<Word> createSortedList(Scanner scanner){
 
         List<Word> wordList = new ArrayList<>();
         int index, currentCount;
         String word;
-
-        // set filepath
-        File file = new File("C:\\Users\\M108212\\IdeaProjects\\codingChallenge\\input.txt");
-        Scanner scanner = null;
-
-        try {
-            scanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
 
         if (scanner != null) {
             while (scanner.hasNextLine()) {
@@ -43,25 +65,21 @@ public class Main {
         wordList.sort(Comparator.comparing(Word::getCount));
         Collections.reverse(wordList);
 
-        for (Word w : wordList) {
-            System.out.println("<" + w.getWord() + "," + w.getCount() + ">");
-        }
-
-        printHistogram(wordList);
-
+        return wordList;
     }
 
-    private static int checkListForWord(List<Word> list, String word){
-        for (Word w: list){
-            if (w.getWord().equals(word)){
-                return list.indexOf(w);
-            }
+    private static Scanner initializeScanner(){
+
+        // set filepath
+        File file = new File("C:\\Users\\M108212\\IdeaProjects\\codingChallenge\\input.txt");
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
-        return -1;
-    }
-
-    private static void printHistogram(List<Word> wordList){
-
+        return scanner;
     }
 }
