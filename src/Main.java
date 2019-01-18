5b32df10c9732e698d148897fe1938522a501aa4
Marfin,Riley E <marfin.riley@heb.com>
2019-01-18 11:55:39 -0600

    added project documentation

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 import java.util.List;
 import java.io.IOException;
 
+/**
+ * Write a program that reads in a paragraph form a file (input.txt) and generates a histogram
+ * of the words used, sorted from most occurrences to least. The output will consist of the word followed by a
+ * pipe character ("|"), a number of equal signs that are proportional to the number of occurrences found in the
+ * text, and the number of occurrences itself. Have the program read in the paragraph defined in
+ * the input.txt file below and generate a histograph stored in an output file (output.txt).
+ */
 public class Main {
 
     /**
