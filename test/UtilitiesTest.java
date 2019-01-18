f086225b37640672f24c5ad05ec42b31e59c04b7
Marfin,Riley E <marfin.riley@heb.com>
2019-01-18 14:17:11 -0600

    added unit tests for utilities

---
+++ test/UtilitiesTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilitiesTest {
+
+    @Test
+    void checkListForWord() {
+    }
+
+    @Test
+    void printHistogram() {
+    }
+
+    @Test
+    void createSortedList() {
+    }
+
+    @Test
+    void initializeScanner() {
+    }
+}
